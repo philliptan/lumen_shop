89f06488d3a839ed67cd133b2dacfd5fb693295c
tan ubuntu <le.viet.tan@softfront.com.vn>
2017-01-12 19:13:56 +0900

    add user transformer

---
+++ app/Transformer/UserTransformer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Transformer;
+
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+
+}
---
+++ tests/app/Transformer/UserTransformerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\App\Transformer;
+
+use TestCase;
+use App\User;
+use App\Transformer\UserTransformer;
+use League\Fractal\TransformerAbstract;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class UserTransformerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test **/            
+    public function it_can_be_initialized()
+    {
+        $subject = new UserTransformer();
+        $this->assertInstanceOf(TransformerAbstract::class, $subject);
+    }
+
+    /** @test **/            
+    public function it_transforms_a_book_model()
+    {
+
+    }
+}
